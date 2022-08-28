59ac04561de5c228dc6a53383ebb660b0eea3a6e
unknown <alexandreroffiaen@gmail.com>
2022-08-29 01:07:34 +0200

    menu responsive

---
+++ script/responsive.js
@@ -0,0 +1,12 @@
+function showResponsiveMenu() {
+    var menu = document.getElementById("topnav_responsive_menu");
+    var icon = document.getElementById("topnav_hamburger_icon");
+    var root = document.getElementById("root");
+    if (menu.className === "") {
+      menu.className = "open";
+      icon.className = "open";
+    } else {
+      menu.className = "";                    
+      icon.className = "";
+    }
+  }
